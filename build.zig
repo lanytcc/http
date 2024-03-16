b6028009dee8814fd2caa1696ae09e025f6ae277
maye174 <96584640+maye174@users.noreply.github.com>
2024-03-16 20:01:31 +0800

    fix bug

--- build.zig
+++ build.zig
@@ -1,23 +1,18 @@
 const std = @import("std");
-const quickjs = @import("../quickjs/build.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
-    const q = quickjs.build_quickjs(b, target);
-    // const http = b.addSharedLibrary(.{ .name = "http" });
     const http = b.addSharedLibrary(.{
         .name = "http",
         .target = target,
         .optimize = .ReleaseSafe,
     });
-    // http.setMode(mode);
 
     http.linkLibC();
-    http.linkLibrary(q);
     http.addIncludePath(.{ .path = "../quickjs" });
-    // http.addLibraryPath(.{ .path = "../quickjs/zig-out/lib" });
-    // http.linkSystemLibrary("quickjs");
+    http.addLibraryPath(.{ .path = "../quickjs/zig-out/lib" });
+    http.linkSystemLibrary("quickjs");
     http.linkSystemLibrary("c");
     http.addCSourceFiles(.{
         .files = &.{ "http.c", "util.c" },
